8b7a2aca6f29e1f10da45c30cd5b616d157c53d8
mzandtheraspberrypi <mzandtheraspberrypi@gmail.com>
2023-09-09 20:27:07 +0000

    updating to deinit

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,19 @@ int main() {
   // I2C change parameters to fit to your LCD
   if (!display.init(OLED_I2C_RESET, OLED_ADAFRUIT_I2C_128x64))
     exit(EXIT_FAILURE);
-  std::cout << "hi" << std::endl;
+  display.begin();
+  // init done
+  display.clearDisplay();   // clears the screen  buffer
+  display.display();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("Hello, world!\n");
+  display.display();
+  std::cout << "showing hi" << std::endl;
+  delay(2000);
+
   size_t width = 128;
   size_t height = 64;
   IMUVisualizer visualizer(width, height);
@@ -41,21 +53,29 @@ int main() {
   double rotation = 0;
 
   while (!EXIT_FLAG) {
+    display.clearDisplay();
 
     CoordinateFrame new_frame =
         visualizer.get_rotated_coordinate_frame(0, 0, rotation);
     std::cout << "original coord\n" << new_frame.frame << std::endl;
-    FlattenedCoordinateFrame flattened_frame =
-        visualizer.flatten_coordinate_frame(new_frame);
-    std::cout << flattened_frame.frame << std::endl;
+    FlattenedCoordinateFrame flattened_frame =  visualizer.flatten_coordinate_frame(new_frame);
 
+    // std::cout << flattened_frame.frame << std::endl;
     int r = static_cast<int>(flattened_frame.frame.rows());
 
-    for (int i = 1; i < r; r += 2) {
+    for (int i = 1; i < r; i += 2) {
+      std::cout << "drawing line " << i << std::endl;
+      double startx = flattened_frame.frame(i - 1, 0);
+      double starty = flattened_frame.frame(i-1, 1);
+      double endx = flattened_frame.frame(i, 0);
+      double endy = flattened_frame.frame(i, 1);
+      std::cout << "  from " << startx << "," << starty << " to " << endx << "," << endy << std::endl;    
       display.drawLine(
           flattened_frame.frame(i - 1, 0), flattened_frame.frame(i - 1, 1),
           flattened_frame.frame(i, 0), flattened_frame.frame(i, 1), 1);
     }
+
+    std::cout << " hi" << std::endl;
     display.display();
 
     rotation += 1 * M_PI / 180;
@@ -64,4 +84,6 @@ int main() {
 
   display.clearDisplay();
   display.display();
+  display.close();
 }
+
