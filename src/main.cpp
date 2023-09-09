0d5f08c964a9706598ee418d614de46ebc60e3bc
mzandtheraspberrypi <mzandtheraspberrypi@gmail.com>
2023-09-09 20:49:52 +0000

    trying some debugging

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,51 @@ void setup_sigint_handler(struct sigaction &sig_int_handler) {
   sigaction(SIGINT, &sig_int_handler, NULL);
 }
 
+void testdrawline(Arduipi_NS::ArduiPi_OLED& display) {  
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, WHITE);
+    display.display();
+  }
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, WHITE);
+    display.display();
+  }
+  delay(1000);
+  
+  display.clearDisplay();
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, WHITE);
+    display.display();
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
+    display.display();
+  }
+  delay(1000);
+  
+  display.clearDisplay();
+  for (int16_t i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
+    display.display();
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
+    display.display();
+  }
+  delay(1000);
+
+  display.clearDisplay();
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, WHITE);
+    display.display();
+  }
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
+    display.display();
+  }
+  delay(1000);
+}
+
 int main() {
 
   struct sigaction sig_int_handler;
@@ -55,7 +100,9 @@ int main() {
   while (!EXIT_FLAG) {
     display.clearDisplay();
 
-    CoordinateFrame new_frame =
+    testdrawline(display);
+
+ /*   CoordinateFrame new_frame =
         visualizer.get_rotated_coordinate_frame(0, 0, rotation);
     std::cout << "original coord\n" << new_frame.frame << std::endl;
     FlattenedCoordinateFrame flattened_frame =  visualizer.flatten_coordinate_frame(new_frame);
@@ -65,18 +112,39 @@ int main() {
 
     for (int i = 1; i < r; i += 2) {
       std::cout << "drawing line " << i << std::endl;
-      double startx = flattened_frame.frame(i - 1, 0);
-      double starty = flattened_frame.frame(i-1, 1);
-      double endx = flattened_frame.frame(i, 0);
-      double endy = flattened_frame.frame(i, 1);
+      int16_t startx = flattened_frame.frame(i - 1, 0);
+      int16_t starty = flattened_frame.frame(i-1, 1);
+      int16_t endx = flattened_frame.frame(i, 0);
+      int16_t endy = flattened_frame.frame(i, 1);
+
+      if (endx > width)
+      {
+        endx = width - 1;
+      }
+      if (endx < 0)
+      {
+        endx = 0;
+      }
+
+      if (endy > height)
+      {
+        endy = height - 1;
+      }
+
+      if (endy < 0)
+      {
+        endy = 0;
+      }
+
       std::cout << "  from " << startx << "," << starty << " to " << endx << "," << endy << std::endl;    
       display.drawLine(
-          flattened_frame.frame(i - 1, 0), flattened_frame.frame(i - 1, 1),
-          flattened_frame.frame(i, 0), flattened_frame.frame(i, 1), 1);
+          startx, starty, endx, endy, 1);
     }
 
     std::cout << " hi" << std::endl;
     display.display();
+    
+    */
 
     rotation += 1 * M_PI / 180;
     delay(LOOP_DELAY_MS);
