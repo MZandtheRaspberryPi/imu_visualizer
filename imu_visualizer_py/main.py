0212cf0bfb1721cb5598bbc91c3d1d18686d35ba
mzandtheraspberrypi <mzandtheraspberrypi@gmail.com>
2023-09-12 23:21:39 +0200

    adding font

--- imu_visualizer_py/main.py
+++ imu_visualizer_py/main.py
@@ -87,7 +87,7 @@ DRAW.rectangle(
 )
 
 # Load default font.
-font = ImageFont.truetype("arial.ttf", FONT_SIZE)
+font = ImageFont.truetype(os.path.join(os.dirname(__file__, )"arial.ttf"), FONT_SIZE)
 
 # Draw Some Text
 text = "Hello World!"
